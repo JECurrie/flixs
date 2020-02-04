ed46f4e560ece2f41b23d6cdda4b606463d2d55b
Joan Currie <jecurrie8@gmail.com>
2020-02-04 15:14:23 -0500

    Form: create records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    def index
-        @movies = ["BugSmash", "Hackathon", "Kata Camp", "Rails User Group"]
-    end     
+def index
+    @movies = ["BugSmash", "Hackathon", "Kata Camp", "Rails User Group"]
+end     
 end
