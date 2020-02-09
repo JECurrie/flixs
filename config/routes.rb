d3c322897c91422f266480ce6537a41716c9e7d1
Joan Currie <jecurrie8@gmail.com>
2020-02-09 06:41:30 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :genres
   root "movies#index"
 
   resources :movies do
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
   resources :users
   get "signup" => "users#new"
 end
+
 
